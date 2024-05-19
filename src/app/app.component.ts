3c0517edd92e97d970255fd8734890077b01c72b
BAyon <ayon.dey@brainiuminfotech.com>
2024-05-20 00:30:54 +0530

    adding files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layouts/header/header.component'; 
+import { ProductComponent } from './product/product.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, ProductComponent, RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'FlipKart';
+  appLogo = 'https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/flipkart-plus_8d85f4.png'
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { ProductComponent } from './product/product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'products', component: ProductComponent },
+];
